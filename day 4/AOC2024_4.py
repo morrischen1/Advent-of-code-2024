1940e1d340e5a86e112d1f5e3cf33119659587d7
Niklas Ku <niklas.ku.4645@student.lu.se>
2024-12-04 12:42:55 +0100

    part 1 attempt

---
+++ day 4/AOC2024_4.py
@@ -0,0 +1,38 @@
+import re
+
+def count(grid, word):
+    count = 0
+    for row in grid:
+        count += len(re.findall(word, row))
+    
+    for col in range(len(grid[0])): 
+        column = ''.join([row[col] for row in grid])
+        count += len(re.findall(word, column))
+
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            diag = ''
+            for i in range(min(len(grid) - r, len(grid[0]) - c)):
+                diag += grid[r + i][c + i]
+                count += len(re.findall(word, diag))
+            
+            diag = ''
+            for i in range(min(len(grid) - r, c + 1)):
+                diag += grid[r + i][c - i]
+                count += len(re.findall(word, diag))
+    
+    return count
+            
+def readFile_ToGrid(filepath):
+    with open(filepath, 'r') as f:
+        lines = [line.strip() for line in f.readlines()]
+    return lines
+
+filepath = "data.txt"
+
+word = "XMAS"
+grid = readFile_ToGrid(filepath)
+
+
+print(count(grid, word))
+
