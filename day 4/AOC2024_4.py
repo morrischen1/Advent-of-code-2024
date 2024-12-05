c765d646ec9cd610c4f8930136892766090248e7
Niklas Ku <niklas.ku.4645@student.lu.se>
2024-12-05 09:25:57 +0100

    .

--- day 4/AOC2024_4.py
+++ day 4/AOC2024_4.py
@@ -1,24 +1,28 @@
 import re
 
-def count(grid, word):
+def count(grid, words):
     count = 0
     for row in grid:
-        count += len(re.findall(word, row))
+        for word in words:
+            count += len(re.findall(word, row))
     
     for col in range(len(grid[0])): 
         column = ''.join([row[col] for row in grid])
-        count += len(re.findall(word, column))
+        for word in words:
+            count += len(re.findall(word, column))
 
     for r in range(len(grid)):
         for c in range(len(grid[0])):
             diag = ''
             for i in range(min(len(grid) - r, len(grid[0]) - c)):
                 diag += grid[r + i][c + i]
+            for word in words:
                 count += len(re.findall(word, diag))
             
             diag = ''
             for i in range(min(len(grid) - r, c + 1)):
                 diag += grid[r + i][c - i]
+            for word in words:
                 count += len(re.findall(word, diag))
     
     return count
@@ -30,7 +34,7 @@ def readFile_ToGrid(filepath):
 
 filepath = "data.txt"
 
-word = "XMAS"
+word = ["XMAS", "SAMX"]
 grid = readFile_ToGrid(filepath)
 
 
